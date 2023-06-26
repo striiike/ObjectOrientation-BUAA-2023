06774fb079122bf7ecf4724a3630e3675d7a8a98
striiike <48015299+striiike@users.noreply.github.com>
2023-06-26 20:39:09 +0800

    update

--- test.bat
+++
@@ -1,5 +0,0 @@
-FOR /L %%A IN (1,1,14) DO (
-  git clone git@gitlab.oo.buaa.edu.cn:oo_homework_2023/oohomework_2023_21373275_hw_%%A.git
-)
-git clone git@gitlab.oo.buaa.edu.cn:oo_homework_2023/oohomework_2023_21373275_hw15.git
-:: git@gitlab.oo.buaa.edu.cn:oo_homework_2023/oohomework_2023_21373275_hw_14.git
